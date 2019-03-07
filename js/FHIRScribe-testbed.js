84a9e341eb59e748b76c30c2e0c53a7eefa91517
rssahsi <rssahsi@users.noreply.github.com>
2019-03-07 18:01:57 -0500

    Update FHIRScribe-testbed.js

--- js/FHIRScribe-testbed.js
+++ js/FHIRScribe-testbed.js
@@ -13,7 +13,7 @@
       if (smart.hasOwnProperty('patient')) {
         var patient = smart.patient;
         var pt = patient.read();
-        console.log(JSON.parse(pt));
+        console.log(JSON.stringify(smart.patient));
         var obv = smart.patient.api.fetchAll({
                     type: 'Observation',
                     query: {
