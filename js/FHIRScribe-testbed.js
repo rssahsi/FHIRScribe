002b7971e64b52eccedc8bb61f9618503d4c95cc
rssahsi <rssahsi@users.noreply.github.com>
2019-03-09 18:40:43 -0500

    Update FHIRScribe-testbed.js

--- js/FHIRScribe-testbed.js
+++ js/FHIRScribe-testbed.js
@@ -37,8 +37,8 @@
         var testkeys = Object.keys(smart);
         console.log("Smart Test Keys: " + testkeys);
 
-        var usr = smart.userId;
-        console.log(smart.userId);
+        var usr = smart.user;
+        console.log("Smart.user:" + smart.user);
         //var usr = user.read();
 
                   /* wait! just how much shit is in that "smart" object we fetched? */
