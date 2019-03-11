99f805d774c81d5d28f44b1126a4bc5e8f421e1a
rssahsi <rssahsi@users.noreply.github.com>
2019-03-11 16:11:49 -0400

    Update FHIRScribe-testbed.js

--- js/FHIRScribe-testbed.js
+++ js/FHIRScribe-testbed.js
@@ -108,7 +108,7 @@ var dataMap = {
 
           //read the AllergyIntolerances Data
           //
-          $.when (smart.api.read({ type: 'AllergyIntolerance', id: patient.id }))
+          $.when (smart.patient.api.read({ type: 'AllergyIntolerance' }))
             .done(function(allergyResult) {
               console.log("");
               console.log("Allergy Data Grab: " + JSON.stringify(allergyResult.data));
