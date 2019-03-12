69ba278e9c20e84f206f8f4fc1e55b14a12ad187
rssahsi <rssahsi@users.noreply.github.com>
2019-03-12 10:10:27 -0400

    Update FHIRScribe-deferred.js

--- js/FHIRScribe-deferred.js
+++ js/FHIRScribe-deferred.js
@@ -57,7 +57,7 @@ var masterMap;
 function loadMasterJSON(callback) {
 var master_grab = new XMLHttpRequest();
 master_grab.overrideMimeType("application/json");
-master_grab.open("GET", "http://rssahsi.github.io/FHIRScribe/PDF/master.json", true);
+master_grab.open("GET", "https://rssahsi.github.io/FHIRScribe/PDF/master.json", true);
 master_grab.onreadystatechange = function () {
     if (master_grab.readyState == 4 && master_grab.status == "200") {
         // Required use of an anonymous callback as .open will NOT return a value but simply returns undefined in asynchronous mode
